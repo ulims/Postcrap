9ab6b5a9f09d0973ab0e2097060f5a5d3035df2f
Agbama <ulimhukaakem@gmail.com>
2022-10-06 02:00:50 +0100

    Assets configuration and shared declarations

---
+++ lib/shared/constants.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+const Color textColor100 = Color(0xff020006);
+const Color textColor80 = Color(0xff2D2B30);
+const Color textColor60 = Color(0xff636266);
+const Color textColor40 = Color(0xff949395);
+const Color textColor20 = Color(0xffC4C4C5);
+const Color textColor10 = Color(0xffDDDDDE);
+const Color textColor3 = Color(0xffEDEDED);
+const Color tlast = Color(0xffF7F7F7);
+const Color primary100 = Color(0xffFA490F);
+const Color success = Color(0xff2ACF89);
+const Color background = Color(0xffFFFFFF);
+const Color error = Color(0xffFE5F5F);
